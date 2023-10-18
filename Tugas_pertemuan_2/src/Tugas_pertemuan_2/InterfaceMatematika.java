7d5e6f5ea50e856cebd44c0ab65e038ea3b5d5c8
Fitriakmaliah <114641149+Fitriakmaliah@users.noreply.github.com>
2023-10-18 14:10:02 +0700

    Add files via upload

---
+++ Tugas_pertemuan_2/src/Tugas_pertemuan_2/InterfaceMatematika.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tugas_pertemuan_2;
+
+/**
+ *
+ * @author ACER i7
+ */
+public interface InterfaceMatematika {
+        int pertambahan(int a, int b);
+        int pengurangan(int a, int b);
+        int perkalian(int a, int b);
+        int pembagian(int a, int b);   
+}
---
+++ Tugas_pertemuan_2/src/Tugas_pertemuan_2/LirikBingo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Tugas_pertemuan_2;
+
+/**
+ *
+ * @author ACER i7
+ */
+public class LirikBingo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String lirik = null;
+        lirik ll = new lirik(lirik);
+        ll.info();
+    }
+    
+}
